1979d7e1eaf5d2255b0977f9acbfcb60c5b9c932
Sebin Benjamin <sebinbenjamin@gmail.com>
2021-04-19 19:29:03 +1200

    feat: update front-end structure

--- src/App/App.js
+++ src/App/App.js
@@ -8,24 +8,14 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <img src={logo} alt="React Logo" />
-      <p>
-        Edit <code>src/App.js</code> and save
-        <h1>{API_URL}</h1> to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-      Max Age is {MAX_AGE}.<FileUpload></FileUpload>
-      <hr></hr>
-      <ShowcaseProfilePic></ShowcaseProfilePic>
-    </div>
+
+  // <HEADER></HEADER>
+  // React router to be put here and all the other PAGES need to be shown based on the route.
+      // - <Home></Home>
+      // - <StudentProjects></StudentProjects>
+      // - <Dashboard></Dashboard>
+  // <FOOTER></FOOTER> 
+  
   );
 }
 
---
+++ src/pages/StudentProjects/StudentProjects.js
@@ -0,0 +1,12 @@
+const StudentProjects= () => {
+  return (
+
+    // <Difficulty></Difficulty>
+    // <ProjectCard></ProjectCard> - create multiple ProjectCard component based on API response
+    // <ProjectCard></ProjectCard> 
+    // <ProjectCard></ProjectCard> 
+    // <ProjectCard></ProjectCard> 
+  );
+}
+
+export default StudentProjects;
